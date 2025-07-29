18ab1d6abc4b760de0249adc43cac45bbcd24c43
Ricky (rcrio) <rchanapplications@gmail.com>
2025-07-29 11:55:04 +0100

    CPP1-3 #done Created a room class with 4 room pointers linked to it, and a list of items per room.

---
+++ room.cpp
@@ -0,0 +1,25 @@
+#include "room.h"
+#include "item.h"
+#include <vector>
+
+
+Room::Room(int id, Room* north, Room* south, Room* west, Room* east) : id(id), north(north), south(south), west(west), east(east) {}
+
+const Room* Room::getNorth() const {
+    return this->north;
+}
+
+const Room* Room::getSouth() const {
+    return this->south;
+}
+
+const Room* Room::getWest() const {
+    return this->west;
+}
+
+const Room* Room::getEast() const {
+    return this->east;
+}
+const std::vector<Item>& Room::getItems() const {
+    return this->items;
+}
