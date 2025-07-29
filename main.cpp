ccabb9886781bb1759d1f053ea9f8b6ac842de00
Ricky (rcrio) <rchanapplications@gmail.com>
2025-07-29 07:09:33 +0100

    First commit, adding base main file

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "Press enter to exit the program." << std::endl;
+    std::cin.get();
+    return 0;
+};
